22ca1d33a42ba7b3401a0f7014aa0c8ae300224d
asel-it <aitmatovaaa@gmail.com>
2024-11-15 12:48:30 +0600

    Update index.js

--- index.js
+++ index.js
@@ -63,9 +63,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Chatbot and AI dialogues
     const dialogues = [
-        { text: "AI: Let's create a plan for your day. What do you need to accomplish?", image: "/images/image1.png" },
-        { text: "AI: Don't forget your meeting at 3 PM.", image: "/images/image2.png" },
-        { text: "AI: How can I assist you with family event planning?", image: "/images/image3.png" }
+        { text: "AI: Let's create a plan for your day. What do you need to accomplish?", image: "image1.png" },
+        { text: "AI: Don't forget your meeting at 3 PM.", image: "image2.png" },
+        { text: "AI: How can I assist you with family event planning?", image: "image3.png" }
     ];
     const chatBox = document.getElementById('animation-box');
     const imageContainer = document.getElementById('image-container');
